ee03ae55c6adc10917c666392f30cfd57fd566c6
Matheus Negrini <mndias29@gmail.com>
2023-12-18 14:09:57 -0300

    esqueci

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import "./index.scss";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
+import carousel_1 from "../../assets/carousel-1.png";
+import carousel_2 from "../../assets/carousel-2.png";
 
 const Carousel = () => {
   return (
     <div className="Carousel">
-      <Splide options={{ type: "loop", pagination: "" }}>
-        <SplideSlide>
-          <h5>test1</h5>
+      <Splide
+        className="splide"
+        options={{ type: "loop", pagination: "inside", drag: "", cover: true }}
+      >
+        <SplideSlide className="slide">
+          <img src={carousel_1} alt="" />
+          <div className="content">
+            <h1>Lorem Ipsum Dolor</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+              dolores eius, similique atque, modi dolore mollitia quasi sit
+              rerum accusantium culpa maiores deserunt cum laboriosam voluptatum
+              dolor suscipit earum voluptates a quia repellendus, voluptate
+              fuga. Id alias saepe esse sit?
+            </p>
+            <a href="#">Agendar Agora</a>
+          </div>
         </SplideSlide>
-        <SplideSlide>
+        <SplideSlide className="slide">
+          <img src={carousel_2} alt="" />
           <h5>test2</h5>
         </SplideSlide>
       </Splide>
